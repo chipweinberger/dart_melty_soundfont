7fbb15c7772d5951e049984defdb36898f74edf5
Chip Weinberger <weinberger.c@gmail.com>
2023-10-04 15:37:29 -0700

    [Example] add soundfont, pubspec.yaml, print all presets

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,18 +1,19 @@
+// ignore_for_file: avoid_print
+
 import 'dart:typed_data'; // for Uint8List
 
+import 'package:dart_melty_soundfont/preset.dart';
 import 'package:flutter/services.dart' show rootBundle;
 
 import 'package:flutter/material.dart';
 import 'package:raw_sound/raw_sound_player.dart';
 
-import 'package:dart_melty_soundfont/preset.dart';
 import 'package:dart_melty_soundfont/synthesizer.dart';
 import 'package:dart_melty_soundfont/synthesizer_settings.dart';
 import 'package:dart_melty_soundfont/audio_renderer_ex.dart';
 import 'package:dart_melty_soundfont/array_int16.dart';
 
-
-String asset = 'assets/akai_steinway.sf2';
+String asset = 'assets/TimGM6mb_but_fixed__piano_.sf2';
 int sampleRate = 44100;
 
 void main() => runApp(const MeltyApp());
@@ -25,7 +26,6 @@ class MeltyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MeltyApp> {
-
   final _rawSound = RawSoundPlayer();
 
   Synthesizer? _synth;
@@ -66,6 +66,21 @@ class _MyAppState extends State<MeltyApp> {
     super.dispose();
   }
 
+  void selectInstrumentPreset(int preset) {
+    _synth!.processMidiMessage(
+        channel: 0,
+        command: 0xC0, // program change
+        data1: _synth!.soundFont.presets[preset].patchNumber,
+        data2: 0);
+
+    _synth!.processMidiMessage(
+      channel: 0,
+      command: 0xB0, // control change
+      data1: 0x00, // bank select
+      data2: _synth!.soundFont.presets[preset].bankNumber,
+    );
+  }
+
   Future<void> _play() async {
     if (_rawSound.isPlaying) {
       return;
@@ -78,19 +93,29 @@ class _MyAppState extends State<MeltyApp> {
     // turnOff all notes
     _synth!.noteOffAll();
 
-    // turnOn some notes
-    int ch = 0;
-    for (Preset p in _synth!.soundFont.presets){
-      // 0xC0 = Program Change
-      _synth!.processMidiMessage(channel:ch % 16, command:0xC0, data1:p.patchNumber, data2:0);
-      _synth!.noteOn(channel: ch % 16, key: 76, velocity: 120);
-      ch++;
+    // print available preset instruments
+    for (int i = 0; i < _synth!.soundFont.presets.length; i++) {
+      Preset p = _synth!.soundFont.presets[i];
+      String instrumentName = p.regions.isNotEmpty ? p.regions[0].instrument.name : "N/A";
+      print('[preset $i] name: ${p.name} instrument: $instrumentName');
     }
 
-    // feed 2 seconds of audio
-    int seconds = 2;
-    ArrayInt16 buf16 = ArrayInt16.zeros(numShorts: sampleRate * seconds);
-    _synth!.renderMonoInt16(buf16);
+    // load instrument
+    selectInstrumentPreset(0);
+
+    // c major scale
+    List<int> notes = [60, 62, 64, 65, 67, 69, 71, 72];
+    int samplesPerNote = sampleRate ~/ 2;
+    ArrayInt16 buf16 = ArrayInt16.zeros(numShorts: samplesPerNote * notes.length);
+    for (int i = 0; i < notes.length; i++) {
+      if (i > 0) {
+        _synth!.noteOff(channel: 0, key: notes[i - 1]);
+      }
+      _synth!.noteOn(channel: 0, key: notes[i], velocity: 120);
+      _synth!.renderMonoInt16(buf16, length: samplesPerNote, offset: samplesPerNote * i);
+    }
+
+    int seconds = (buf16.bytes.lengthInBytes / 2) ~/ sampleRate;
     await _rawSound.feed(buf16.bytes.buffer.asUint8List());
     await Future.delayed(Duration(seconds: seconds));
     await _stop();
@@ -98,7 +123,7 @@ class _MyAppState extends State<MeltyApp> {
 
   Future<void> _stop() async {
     if (_rawSound.isPlaying == false) {
-        return;
+      return;
     }
     await _rawSound.stop();
     setState(() {});
@@ -127,7 +152,7 @@ class _MyAppState extends State<MeltyApp> {
     }
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: const Text('Day Selector')),
+        appBar: AppBar(title: const Text('Soundfont')),
         body: child,
       )
     );
