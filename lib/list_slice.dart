72ad81f67e29cfd6db716fa99a360459e77d8ce2
Akmal <>
2024-10-27 17:07:34 +0800

    Improve list slicing

--- lib/list_slice.dart
+++ lib/list_slice.dart
@@ -5,22 +5,34 @@ import 'dart:collection';
 class ListSlice<E> extends ListBase<E> {
   final List<E> _list;
   final int start;
+  final int _length;
+
+  ListSlice(this._list, this.start, int length)
+      : assert(start + length <= _list.length, "Slice length must be less than list length"),
+        _length = length;
 
   @override
-  int length;
+  int get length => _length;
 
-  ListSlice(this._list, this.start, this.length): assert(start + length <= _list.length, "Slice length must be less than list length");
+  set length(int value) => throw UnsupportedError('Setting length is not supported on list slice');
 
   @override
   E operator [](int index) {
+    if (index < 0 || index >= length) {
+      throw RangeError.index(index, this); // Check bounds
+    }
     return _list[index + start];
   }
 
   @override
   void operator []=(int index, E value) {
+    if (index < 0 || index >= length) {
+      throw RangeError.index(index, this); // Check bounds
+    }
     _list[index + start] = value;
   }
 }
+
 
 extension ListSliceExtension<E> on List<E> {
   ListSlice<E> slice(int start, int length) {
